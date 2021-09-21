e38f3fed9d45e60e7247eb5aa259637a044043d9
vadim_urazov_373 <vadim.urazov@gmail.com>
2021-09-21 13:15:43 +0600

    SimplePaint

---
+++ SimplePaint/app/src/main/java/com/example/simplepaint/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.simplepaint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Draw2D draw2D = new Draw2D(this);
+        setContentView(draw2D);
+    }
+}
